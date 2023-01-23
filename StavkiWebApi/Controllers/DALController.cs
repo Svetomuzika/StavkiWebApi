33488e416ce2e6718ec2fc5fc4602095a133af4d
Svetomuzika <sozonov_artem@mail.ru>
2023-01-23 20:53:13 +0500

    Добавлен метод получения суммы заявки С НДС

--- StavkiWebApi/Controllers/DALController.cs
+++ StavkiWebApi/Controllers/DALController.cs
@@ -95,10 +95,10 @@ namespace StavkiWebApi.Controllers
         [HttpGet("Stavki/GetAllPuncts")]
         public IEnumerable<string> GetAllPuncts()
         {
-            var a = new List<string>() { "Екатеринбург" };
+            var a = new List<string>();
 
-            var a1 = unitOfWork.BlizMezhGorodSNDS.GetAll().Select(x => x.City).ToList();
-            var a2 = unitOfWork.MezhgorodSNDS.GetAll().Select(x => x.City).ToList();
+            var a1 = unitOfWork.BlizMezhGorodSNDS.GetAll().Select(x => x.City);
+            var a2 = unitOfWork.MezhgorodSNDS.GetAll().Select(x => x.City);
 
             a.AddRange(a1);
             a.AddRange(a2);
@@ -122,6 +122,41 @@ namespace StavkiWebApi.Controllers
         public IEnumerable<Request> GetAllRequestsByClientId(int clientId)
         {
             return unitOfWork.Requests.GetAll().Where(x => x.ClientId == clientId);
+        }
+
+        [HttpGet("Requests/GetSum")]
+        public float GetSum(float weight, string city)
+        {
+            var gorod = unitOfWork.Gorod.GetAll();
+            var bliz = unitOfWork.BlizMezhGorodSNDS.GetAll();
+            var mezh = unitOfWork.MezhgorodSNDS.GetAll();
+            float? result = 0;
+
+            if (bliz.Select(x => x.City).Contains(city))
+            {
+                if (weight < 24)
+                    result = bliz.Where(x => x.City == city).Single().Ft20;
+
+                if (weight >= 24 && weight <= 27)
+                    result = bliz.Where(x => x.City == city).Single().Ft40;
+
+                if (weight > 27)
+                    result = bliz.Where(x => x.City == city).Single().Ot24Do30Tn;
+            }
+
+            if (mezh.Select(x => x.City).Contains(city))
+            {
+                if (weight < 24)
+                    result = mezh.Where(x => x.City == city).Single().Do24;
+
+                if (weight >= 24 && weight <= 27)
+                    result = mezh.Where(x => x.City == city).Single().Ot24Do27;
+
+                if (weight > 27)
+                    result = mezh.Where(x => x.City == city).Single().Ot27;
+            }
+
+            return result ??= 0;
         }
     }
 }
