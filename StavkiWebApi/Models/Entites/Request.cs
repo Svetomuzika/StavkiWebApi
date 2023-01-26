7c6735866e80104515c8f691bcd1b781fe4fdf33
Svetomuzika <sozonov_artem@mail.ru>
2023-01-27 00:12:34 +0500

    new

--- StavkiWebApi/Models/Entites/Request.cs
+++ StavkiWebApi/Models/Entites/Request.cs
@@ -6,5 +6,8 @@ namespace StavkiWebApi.Models.Entites
     public class Request : RequestDomain
     {
         public Client Client { get; set; }
+
+        public string Responsible { get; set; } = "Яковский А.А";
+
     }
 }
