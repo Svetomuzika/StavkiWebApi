504a6ef0ed85e0f389ad111c1c69fb37244b42a7
Svetomuzika <sozonov_artem@mail.ru>
2023-05-16 09:20:45 +0500

    Мигрейт CityType

--- Stavki.Infrastructure/EF/Domains/Base/BaseDomain.cs
+++ Stavki.Infrastructure/EF/Domains/Base/BaseDomain.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Stavki.Data.Data.Enums;
-
-namespace Stavki.Infrastructure.EF.Domains.Base
+﻿namespace Stavki.Infrastructure.EF.Domains.Base
 {
     public abstract class BaseDomain
     {
         public int Id { get; set; }
-
-        public CityType CityType { get; set; }
     }
 }
---
+++ Stavki.Infrastructure/Migrations/20230516042027_Initial13.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stavki.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityType",
+                table: "NearInCityNDS",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityType",
+                table: "NearInCity",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityType",
+                table: "InCityNDS",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityType",
+                table: "InCity",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CityType",
+                table: "NearInCityNDS");
+
+            migrationBuilder.DropColumn(
+                name: "CityType",
+                table: "NearInCity");
+
+            migrationBuilder.DropColumn(
+                name: "CityType",
+                table: "InCityNDS");
+
+            migrationBuilder.DropColumn(
+                name: "CityType",
+                table: "InCity");
+        }
+    }
+}
