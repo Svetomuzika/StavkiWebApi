39efcc83873cf188b87cde2e44e5451a19678b6f
Svetomuzika <sozonov_artem@mail.ru>
2023-05-22 21:18:01 +0500

    Добавлена миграции

---
+++ Stavki.Infrastructure/Migrations/20230522161734_Initial14.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stavki.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResponsibleUser",
+                table: "Requests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResponsibleUserId",
+                table: "Requests",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResponsibleUser",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "ResponsibleUserId",
+                table: "Requests");
+        }
+    }
+}
