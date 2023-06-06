5e816de9d3e0fc5d7ae490e5c68056e63ca9057b
Svetomuzika <sozonov_artem@mail.ru>
2023-06-06 19:23:06 +0500

    123

--- Stavki.Infrastructure/SignalR/ChatHub.cs
+++ Stavki.Infrastructure/SignalR/ChatHub.cs
@@ -5,7 +5,7 @@ namespace Stavki.Infrastructure.SignalR
 {
     public class ChatHub : Hub
     {
-        public async Task Send(CommentInfo comment)
+        public async Task Send(string comment)
         {
             await Clients.All.SendAsync("Receive", comment);
         }
