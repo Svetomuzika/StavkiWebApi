e40ed651869d420bc47cd20a6aa7d6af962b1045
Svetomuzika <sozonov_artem@mail.ru>
2023-06-06 21:52:23 +0500

    123

--- Stavki.Infrastructure/SignalR/ChatHub.cs
+++ Stavki.Infrastructure/SignalR/ChatHub.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Stavki.Data.Data;
+using Stavki.Infrastructure.EF.Domains;
+using Stavki.Infrastructure.Services.Interfaces;
 
 namespace Stavki.Infrastructure.SignalR
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string comment)
+        private readonly IRequestService _requestService;
+
+
+        public ChatHub(IRequestService requestService)
         {
-            await Clients.All.SendAsync("Receive", comment);
+            _requestService = requestService;
+        }
+
+        public async Task Send(CommentInfo comment)
+        {
+            var res = _requestService.AddComment(comment);
+
+            var comm = new CommentDomain
+            {
+                RequestId = comment.RequestId,
+                UserId = comment.UserId,
+                CreateDate = DateTime.Now,
+                Text = comment.Comment,
+            };
+
+            await Clients.All.SendAsync("Receive", comm);
         }
     }
 }
